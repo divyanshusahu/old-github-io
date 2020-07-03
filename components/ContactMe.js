6444eadcd3108731dd7833757acf4887764dd1ed
divyanshusahu <dsahu1997@gmail.com>
2020-07-03 19:13:26 +0530

    linkedin added

--- components/ContactMe.js
+++ components/ContactMe.js
@@ -2,6 +2,7 @@ import {
   AiOutlineGithub,
   AiOutlineTwitter,
   AiOutlineMail,
+  AiOutlineLinkedin,
 } from "react-icons/ai";
 
 function ContactMe() {
@@ -24,16 +25,22 @@ function ContactMe() {
             <figcaption className="text-center mt-2">Github</figcaption>
           </figure>
         </a>
-        <a href="https://twitter.com/divyan5hu" className="ml-8">
+        <a href="https://twitter.com/divyan5hu" className="ml-4 sm:ml-8">
           <figure>
             <AiOutlineTwitter size="4em" />
             <figcaption className="text-center mt-2">Twitter</figcaption>
           </figure>
         </a>
-        <a href="mailto:dsahu1997@gmail.com" className="ml-8">
+        <a href="mailto:dsahu1997@gmail.com" className="ml-4 sm:ml-8">
           <figure>
             <AiOutlineMail size="4em" />
             <figcaption className="text-center mt-2">Mail</figcaption>
+          </figure>
+        </a>
+        <a href="https://www.linkedin.com/in/divyanshu-sahu/" className="ml-4 sm:ml-8">
+          <figure>
+            <AiOutlineLinkedin size="4em" />
+            <figcaption className="text-center mt-2">LinkedIn</figcaption>
           </figure>
         </a>
       </section>
